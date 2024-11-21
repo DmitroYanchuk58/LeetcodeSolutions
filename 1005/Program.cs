f2624666408c2a4069c3583ca89297fc32cf83ad
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2024-11-21 14:45:50 +0200

    Made solution for 1005. Maximize Sum Of Array After K Negations

---
+++ 1005/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = { 4, 2, 3 };
+var result = LargestSumAfterKNegations(nums, 1);
+Console.WriteLine(result);
+static int LargestSumAfterKNegations(int[] nums, int k)
+{
+    int numberForWhile = 0;
+    while (numberForWhile < k)
+    {
+        int minIndex = Array.IndexOf(nums, nums.Min());
+
+        nums[minIndex] *= -1;
+
+        numberForWhile++;
+    }
+    return nums.Sum();
+}
+
