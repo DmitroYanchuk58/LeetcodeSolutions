798d6102ae22d3636587cd15711da4d9496044f4
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2024-11-23 18:12:06 +0200

    Made solution for 1945. Sum of Digits of String After Convert

---
+++ 1945/1945/Program.cs
@@ -0,0 +1,31 @@
+﻿var num=GetLucky("zbax",2);
+Console.WriteLine(num);
+static int GetLucky(string s, int k)
+{
+    int result = 0;
+
+    foreach (char c in s)
+    {
+        int numValue = c-'a'+1; 
+        while (numValue > 0)
+        {
+            result += numValue % 10; 
+            numValue /= 10;         
+        }
+    }
+
+    for (int i = 1; i < k; i++)
+    {
+        int newResult = 0;
+        while (result > 0)
+        {
+            newResult += result % 10; 
+            result /= 10;             
+        }
+        result = newResult;          
+    }
+
+    return result;
+}
+
+
