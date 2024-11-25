0a4e6e249812df2e5e365a81ec92b096a375d1f2
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2024-11-25 14:04:39 +0200

    Made solution for 2047. Number of Valid Words in a Sentence

---
+++ 2047/2047/Program.cs
@@ -0,0 +1,70 @@
+﻿var count = CountValidWords("  okkx3el1rmzwqfimm  jcn vham ");
+Console.WriteLine(count);
+static int CountValidWords(string sentence)
+{
+    if (string.IsNullOrEmpty(sentence))
+    {
+        return 0;
+    }
+
+    string[] tokens = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+    int result = 0;
+    foreach (string word in tokens)
+    {
+        if (IsWordValid(word))
+        {
+            result++;
+        }
+    }
+
+    return result;
+}
+static bool IsWordValid(string word)
+{
+    
+    if (string.IsNullOrEmpty(word))
+    {
+        return false;
+    }
+
+    bool hasHyphen = false;
+    bool hasPunctuation = false;
+
+    for (int i = 0; i < word.Length; i++)
+    {
+        char c = word[i];
+
+        if (char.IsDigit(c))
+        {
+            return false;
+        }
+
+        if (c == '-')
+        {
+            if (hasHyphen || i == 0 || i == word.Length - 1 || !char.IsLower(word[i - 1]) || !char.IsLower(word[i + 1]))
+            {
+                return false;
+            }
+            hasHyphen = true;
+        }
+
+        if (c == '!' || c == '.' || c == ',')
+        {
+            if (hasPunctuation || i != word.Length - 1)
+            {
+                return false;
+            }
+            hasPunctuation = true;
+        }
+
+        if (!char.IsLower(c) && c != '-' && c != '!' && c != '.' && c != ',')
+        {
+            return false;
+        }
+    }
+
+    return true;
+
+}
