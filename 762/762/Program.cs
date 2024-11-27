06308ff7c642e199e60c6e2ae5435f9be2b35af4
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2024-11-27 14:27:47 +0200

    Made solution for 762. Prime Number of Set Bits in Binary Representation

---
+++ 762/762/Program.cs
@@ -0,0 +1,29 @@
+﻿var result=CountPrimeSetBits(6, 10);
+Console.WriteLine(result);
+static int CountPrimeSetBits(int left, int right)
+{
+    int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19 };
+    int result = 0;
+
+    for (int i = left; i <= right; i++)
+    {
+        if (primes.Contains(CountSetBits(i)))
+        {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+static int CountSetBits(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        count += n & 1; 
+        n >>= 1;       
+    }
+    return count;
+}
+
